a4c3bd2a0c2eaa0bd16273ffa2d033704e20aa9e
Lneto <fioravantelucio@gmail.com>
2026-03-22 22:29:04 -0300

    feat: adição do exercício 03 - diferença de produto

---
+++ src/estrutura_sequencial/Exercicio03DiferencaDeProduto.java
@@ -0,0 +1,40 @@
+/*
+ * Exercício 03 - Diferença de Produto
+ *
+ * Objetivo:
+ * Ler quatro valores inteiros A, B, C e D.
+ * Calcular e mostrar a diferença do produto de A e B pelo produto de C e D.
+ *
+ * Fórmula:
+ * DIFERENCA = (A * B - C * D)
+ */
+
+package estrutura_sequencial;
+
+import java.util.Scanner;
+
+public class Exercicio03DiferencaDeProduto {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    //Variáveis.
+    int a, b, c, d, diferenca;
+
+    //Entrada de dados.
+    System.out.println("Digite quatro valores inteiros: ");
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c = sc.nextInt();
+    d = sc.nextInt();
+
+    //Processamento/Cálculo.
+    diferenca = (a * b - c * d);
+
+    //Saída de dados
+    System.out.printf("DIFERENCA = %d%n", diferenca);
+
+    sc.close();
+  }
+}
